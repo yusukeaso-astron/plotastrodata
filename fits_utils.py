4170399a8811e7c85527b8d56a66a9929ae34c35
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2022-02-10 17:17:04 +0900

    some names changed

--- fits_utils.py
+++ fits_utils.py
@@ -3,53 +3,72 @@ import numpy as np
 from astropy.io import fits
 from astropy import constants, units, wcs
 
-from mytools import listing, coord2xy
+from other_utils import listing, coord2xy
 
 
-def Jy2K(header):
-    bmaj = np.radians(header['BMAJ'])
-    bmin = np.radians(header['BMIN'])
-    bpa = np.radians(header['BPA'])
-    omega = bmaj * bmin * np.pi / 4. * np.log(2.)
-    if 'RESTFREQ' in header.keys(): freq = header['RESTFREQ']
-    if 'RESTFRQ' in header.keys(): freq = header['RESTFRQ']
+
+def Jy2K(header = None, bmaj: float = None, bmin: float = None,
+         freq: float = None) -> float:
+    """Calculate a conversion factor in the unit of K/Jy.
+
+    Args:
+        header (optional): astropy.io.fits.open('a.fits')[0].header
+                           Defaults to None.
+        bmaj (float, optional): beam major axis in degree. Defaults to None.
+        bmin (float, optional): beam minor axis in degree. Defaults to None.
+        freq (float, optional): rest frequency in Hz. Defaults to None.
+
+    Returns:
+        float: the conversion factor in the unit of K/Jy.
+    """
+    if not header is None:
+        bmaj, bmin = header['BMAJ'], header['BMIN']
+        if 'RESTFREQ' in header.keys(): freq = header['RESTFREQ']
+        if 'RESTFRQ' in header.keys(): freq = header['RESTFRQ']
+    omega = bmaj * bmin * np.radians(1)**2 * np.pi / 4. * np.log(2.)
     lam = constants.c.to('m/s').value / freq
     a = units.Jy.to('J*s**(-1)*m**(-2)*Hz**(-1)') \
         * lam**2 / 2. / constants.k_B.to('J/K').value / omega
     return a
 
 
-class MyFits:
-    def __init__(self, fitsimage):
+class FitsData:
+    """For practical treatment of data in a FITS file."""
+    def __init__(self, fitsimage: str):
         self.fitsimage = fitsimage
 
-    def gen_header(self):
+    def gen_header(self) -> None:
         self.header = fits.open(self.fitsimage)[0].header
 
-    def gen_beam(self, ang='deg'):
+    def gen_beam(self, ang: str = 'deg') -> None:
         self.bmaj, self.bmin, self.bpa = 0, 0, 0
         h = fits.open(self.fitsimage)[0].header
         if np.all(np.isin(['BMAJ', 'BMIN', 'BPA'], list(h.keys()))):
-            u = 1
-            if ang == 'arcsec': u = 3600.
-            elif ang in ['rad', 'radian']: u = np.radians(1.)
+            if ang == 'arcsec':
+                u = 3600.
+            elif ang in ['rad', 'radian']:
+                u = np.radians(1.)
+            else:
+                u = 1
             self.bmaj = h['BMAJ'] * u
             self.bmin = h['BMIN'] * u
             self.bpa = h['BPA']
 
-    def gen_data(self, Tb=False, log=False, drop=False):
+    def gen_data(self, Tb: bool = False, log: bool = False,
+                 drop: bool = False) -> None:
         self.data = []
         c = fits.open(self.fitsimage)[0]
-        h = c.header
-        d = c.data
+        h, d = c.header, c.data
         if drop == True: d = np.squeeze(d)
-        if Tb == True: d *= Jy2K(h)
+        if Tb == True: d *= Jy2K(header=h)
         if log == True: d = np.log10(d.clip(np.min(d[d > 0]), None))
         self.data = d
 
-    def gen_grid(self, axes=(0, 1, 2), ang='deg', vel='km/s',
-                 restfrq=0, center=''):
-        h = fits.open(self.fitsimage)[0].header
+    def gen_grid(self, axes: tuple = (0, 1, 2),
+                 ang: str = 'deg', vel: str = 'km/s',
+                 restfrq: float = 0, center: str = '') -> None:
+        if not hasattr(self, 'header'): self.gen_header()
+        h = self.header
         self.x, self.y, self.v = [], [], []
         self.dx, self.dy, self.dv = [], [], []
         g = []
@@ -58,14 +77,14 @@ class MyFits:
                 i = str(i + 1)
                 s = np.arange(h['NAXIS' + i])
                 if ang in ['abspix', 'abspixel']: g.append(s)
-                s = (s - h['CRPIX' + i] + 1) * h['CDELT' + i] + h['CRVAL' + i]
-                if ang in ['deg', 'degree', 'absdeg', 'absdegree']: g.append(s)
+                s = (s - h['CRPIX'+i]+1) * h['CDELT'+i] + h['CRVAL'+i]
+                if ang in ['deg','degree','absdeg','absdegree']: g.append(s)
                 if center != '':
                     cx, cy = coord2xy(center)
                     cntr = cx if i == '1' else cy
                 else:
                     cntr = h['CRVAL' + i]
-                s = s - cntr
+                s -= cntr
                 if ang in ['reldeg', 'reldegree']: g.append(s)
                 if ang in ['pix', 'pixel', 'relpix', 'relpixel']:
                     g.append(np.round(s / h['CDELT' + i]).astype(np.int64))
@@ -80,40 +99,40 @@ class MyFits:
             if h['NAXIS3'] > 1:
                 s = np.arange(h['NAXIS3'])
                 if vel in ['abspix', 'abspixel']: g.append(s)
-                s = (s - h['CRPIX3'] + 1) * h['CDELT3'] + h['CRVAL3']
+                s = (s-h['CRPIX3']+1) * h['CDELT3'] + h['CRVAL3']
                 if vel in ['Hz', 'absHz']: g.append(s)
                 freq = 0
                 if 'RESTFREQ' in h.keys(): freq = h['RESTFREQ']
                 if 'RESTFRQ' in h.keys(): freq = h['RESTFRQ']
                 if restfrq > 0: freq = restfrq
-                s = s - freq
+                s -= freq
                 if vel in ['relHz']: g.append(s)
                 if vel in ['pix', 'pixel', 'relpix', 'relpixel']:
                     g.append(np.round(s / h['CDELT3']).astype(np.int64))
                 if freq > 0:
                     s = -s / freq * constants.c.to('m/s').value
-                    if vel in ['m/s']: g.append(s)
+                    if vel in ['m/s', 'M/S']: g.append(s)
                     s = s / 1e3
-                    if vel in ['km/s']: g.append(s)
+                    if vel in ['km/s', 'KM/S']: g.append(s)
                 self.v = g[2]
                 self.dv = g[2][1] - g[2][0]
 
 
-def fits2data(fitsimage, Tb, center=''):
+def fits2data(fitsimage: str, Tb: bool = False, center: str = '') -> list:
     fitsimage = listing(fitsimage)
     d, x, b = [], [], []
     for f in fitsimage:
-        a = MyFits(f)
+        a = FitsData(f)
         a.gen_data(Tb=Tb, drop=True)
         d.append(a.data)
-        a.gen_grid(ang='arcsec', center=center)
-        x.append([a.x, a.y, a.v])
+        a.gen_grid(ang='arcsec', vel='km/s', center=center)
+        x.append([a.x, a.y, a.v])  # arcsec, arcsec, km/s
         a.gen_beam(ang='arcsec')
-        b.append([a.bmaj, a.bmin, a.bpa])
+        b.append([a.bmaj, a.bmin, a.bpa])  # arcsec, arcsec, deg
     return [d, x, b]
 
 
-def read_bunit(fitsimage='', bunit=''):
+def read_bunit(fitsimage: str = '', bunit: str = ''):
     fitsimage, bunit = listing(fitsimage, bunit)
     a = []
     for f, b in zip(fitsimage, bunit):
@@ -123,8 +142,9 @@ def read_bunit(fitsimage='', bunit=''):
     return a
 
 
-def data2fits(d=None, h={}, crpix=None, crval=None, cdelt=None, ctype=None,
-              fitsname='test', foraplpy=False):
+def data2fits(d: list = None, h: dict = {}, crpix: int = None,
+              crval: int = None, cdelt: float = None, ctype: str = None,
+              fitsname: str = 'test', foraplpy: bool = False):
     ctype0 = ["RA---AIR", "DEC--AIR", "VELOCITY"]
     if foraplpy:
         w = wcs.WCS(naxis=2)
@@ -154,13 +174,13 @@ def data2fits(d=None, h={}, crpix=None, crval=None, cdelt=None, ctype=None,
     hdu.writeto(fitsname.replace('.fits', '') + '.fits', overwrite=True)
 
 
-def hdu4aplpy(fitslist, data):
+def hdu4aplpy(fitslist: list, data: list) -> list:
     fitslist = listing(fitslist)
     c = []
     for n, d in zip(fitslist, data):
         h = fits.open(n)[0].header
-        data2fits(d=d, h=h, foraplpy=True, fitsname='0')
-        c.append(fits.open('0.fits')[0])
-        subprocess.run(shlex.split('rm 0.fits'))
+        data2fits(d=d, h=h, foraplpy=True, fitsname='hdu4aplpy')
+        c.append(fits.open('hdu4aplpy.fits')[0])
+        subprocess.run(shlex.split('rm hdu4aplpy.fits'))
     return c
 
