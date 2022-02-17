81872f463007727000927641caad376af1007a73
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2022-02-17 18:07:18 +0900

    minor

--- example.py
+++ example.py
@@ -1,31 +1,37 @@
-from plotastrodata.channels import chan_fits
-from plotastrodata.images import image_fits
-from plotastrodata.momentmaps import mom_fits
-from plotastrodata.pvdiagrams import pv_fits
-from plotastrodata.lineprofiles import lineprofile
+from plotastrodata.plot_utils import plotastrodata as pad
 
+pre = 'testFITS/'
+# 2D case
+pa = pad(rmax=0.8, center='04h04m43.07s 26d18m56.20s')
+pa.add_color(fitsimage=pre+'test2D.fits', Tb=True, cblabel='Tb (K)')
+pa.add_contour(fitsimage=pre+'test2D_2.fits', colors='r', sigma=5e-3)
+pa.add_contour(fitsimage=pre+'test2D.fits', skip=2, sigma=5e-3)
+pa.add_segment(ampfits=pre+'test2Damp.fits',
+               angfits=pre+'test2Dang.fits', skip=3)
+pa.add_scalebar(length=50 / 140, label='50 au')
+pa.add_label([[0.3, 0.3]], slist=['label'])
+pa.add_marker([[0.7, 0.7]])
+pa.add_line([[0.5, 0.5], [0.6, 0.6]], anglelist=[60, 60], rlist=[0.5, 0.5])
+pa.add_arrow([[0.4, 0.4]], anglelist=[150], rlist=[0.5])
+pa.add_ellipse([[0.2, 0.8]], majlist=[0.4], minlist=[0.2], palist=[45])
+pa.set_axis()
+pa.savefig()
+pa.show()
 
-chan_fits(colorfits='test_input.fits',
-          contourfits='test_input2.fits',
-          rmax=0.8, nrows=3, ncols=5, nskipv=4, vmin=-4.7, vmax=4.7, 
-          scalebar={'length':50 / 140, 'label':'50 au'},
-          savefig='test_chan.png', show=True)
-
-image_fits(colorfits='test_input2.fits',
-           contourfits='test_input2.fits',
-           rmax=0.8,
-           scalebar={'length':50 / 140, 'label':'50 au'},
-           savefig='test_image.png', show=True)
-
-mom_fits(vlim=(-5, 5), cubefits='test_input.fits',
-         rmax=0.8, scalebar={'length':50 / 140, 'label':'50 au'},
-         savefig='test_mom01.png', show=True)
-
-pv_fits(length=1.6, width=0, pa=60,
-        colorfits='test_input.fits',
-        contourfits='test_input.fits',
-        savefig='test_pv.png', show=True)
-
-lineprofile(fitsimage='test_input.fits', coords=['04h04m43.1s 26d18m56.2s'],
-            radius='point', unit='K',
-            ylabel='K', savefig='test_prof.png', show=True)
+# 3D case
+pa = pad(rmax=0.8, center='04h04m43.07s 26d18m56.20s',
+         fitsimage=pre+'test3D.fits', vmin=-5, vmax=5, vskip=2)
+pa.add_color(fitsimage=pre+'test3D.fits', log=True)
+pa.add_contour(fitsimage=pre+'test3D.fits', colors='r')
+pa.add_contour(fitsimage=pre+'test2D_2.fits', colors='b')
+pa.add_segment(ampfits=pre+'test2Damp.fits',
+               angfits=pre+'test2Dang.fits', skip=3)
+pa.add_scalebar(length=50 / 140, label='50 au')
+pa.add_label([[0.3, 0.3]], slist=['label'], include_chan=[0,1,2])
+pa.add_marker(['04h04m43.07s 26d18m56.20s'], [3,4,5])
+pa.add_line([[0.5, 0.5], [0.6, 0.6]], anglelist=[60, 60], rlist=[0.5, 0.5], include_chan=[6,7,8])
+pa.add_arrow([[0.4, 0.4]], anglelist=[150], rlist=[0.5], include_chan=[9,10,11])
+pa.add_ellipse([[0.2, 0.8]], majlist=[0.4], minlist=[0.2], palist=[45], include_chan=[12,13,14])
+pa.set_axis(grid={})
+pa.savefig()
+pa.show()
---
+++ old/example.py
@@ -0,0 +1,31 @@
+from plotastrodata.channels import chan_fits
+from plotastrodata.images import image_fits
+from plotastrodata.momentmaps import mom_fits
+from plotastrodata.pvdiagrams import pv_fits
+from plotastrodata.lineprofiles import lineprofile
+
+
+chan_fits(colorfits='test_input.fits',
+          contourfits='test_input2.fits',
+          rmax=0.8, nrows=3, ncols=5, nskipv=4, vmin=-4.7, vmax=4.7, 
+          scalebar={'length':50 / 140, 'label':'50 au'},
+          savefig='test_chan.png', show=True)
+
+image_fits(colorfits='test_input2.fits',
+           contourfits='test_input2.fits',
+           rmax=0.8,
+           scalebar={'length':50 / 140, 'label':'50 au'},
+           savefig='test_image.png', show=True)
+
+mom_fits(vlim=(-5, 5), cubefits='test_input.fits',
+         rmax=0.8, scalebar={'length':50 / 140, 'label':'50 au'},
+         savefig='test_mom01.png', show=True)
+
+pv_fits(length=1.6, width=0, pa=60,
+        colorfits='test_input.fits',
+        contourfits='test_input.fits',
+        savefig='test_pv.png', show=True)
+
+lineprofile(fitsimage='test_input.fits', coords=['04h04m43.1s 26d18m56.2s'],
+            radius='point', unit='K',
+            ylabel='K', savefig='test_prof.png', show=True)
