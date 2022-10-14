3db8f3b07f80b437fbfeed4379f3f11a67cdb97b
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2022-10-14 18:47:28 +0900

    test

--- plotastrodata/analysis_utils.py
+++ plotastrodata/analysis_utils.py
@@ -157,9 +157,9 @@ class AstroData():
         Det = 4 * beta**2 + (alpha - gamma)**2
         maj2inv = (alpha + gamma - np.sqrt(Det)) / 2
         min2inv = (alpha + gamma + np.sqrt(Det)) / 2
-        print(maj2inv, min2inv)
         bmaj_new, bmin_new = 1 / np.sqrt(maj2inv), 1 / np.sqrt(min2inv)
         self.beam = np.array([bmaj_new, bmin_new, np.degrees(bpa_new)])
+        print(self.beam)
     
     def profile(self, coords: list = [], xlist: list = [], ylist: list = [],
                 ellipse: list = None, flux: bool = False, width: int = 1,
