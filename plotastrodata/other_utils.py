83acd5e912f4b850dc8c04628fb0004483013623
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2023-02-08 14:33:41 +0900

    default of curve_fit

--- plotastrodata/other_utils.py
+++ plotastrodata/other_utils.py
@@ -168,7 +168,7 @@ def estimate_rms(data: list, sigma: float or str = 'hist') -> float:
         def g(x, s, c, R2):
             xn = (x - c) / np.sqrt(2) / s
             return (erf(xn) - erf(xn * np.exp(-R2))) / (2 * R2 * (x - c))
-        popt, _ = curve_fit(g, hbin / s0, hist * s0, p0=[1, 0, 0.5])
+        popt, _ = curve_fit(g, hbin / s0, hist * s0, p0=[1, 0, 0.1])
         pbcor = ''
         if np.sqrt(popt[2]) * np.sqrt(np.log(2)) < 1:
             g = lambda x, s, c: \
