b94bceb8b49dfb601bd25824429886f4fa39e030
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2023-06-15 16:33:34 +0900

    Added path '..'

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, '/Users/yusukeaso/C_programs/Programs/plotastrodata/plotastrodata')
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
