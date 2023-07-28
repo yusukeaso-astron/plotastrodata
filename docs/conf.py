73c5307475972edd7abb66f95415899bc99749a3
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2023-07-28 13:34:37 +0900

    Sphinxdoc updated

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, '/Users/yusukeaso/C_programs/Programs/plotastrodata/plotastrodata')
+sys.path.insert(0, '../plotastrodata')
 sys.path.insert(0, os.path.abspath('..'))
 
 
