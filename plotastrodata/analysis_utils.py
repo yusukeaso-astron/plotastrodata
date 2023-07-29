8b8bbe437046e03bdfbe0040b8fbb26d2ecc8ed3
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2023-07-29 23:55:49 +0900

    Capitalize in Jy2K in AstroData

--- plotastrodata/analysis_utils.py
+++ plotastrodata/analysis_utils.py
@@ -266,8 +266,8 @@ class AstroData():
     def Jy2K(self):
         """Convert Jy/beam to K (brightness temperature).
         """
-        header = {'bmaj':self.beam[0] / 3600,
-                  'bmin':self.beam[1] / 3600,
+        header = {'BMAJ':self.beam[0] / 3600,
+                  'BMIN':self.beam[1] / 3600,
                   'CDELT1':(self.x[1] - self.x[0]) / 3600,
                   'CUNIT1':'DEG',
                   'RESTFREQ':self.restfrq}
