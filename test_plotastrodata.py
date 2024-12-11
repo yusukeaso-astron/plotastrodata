c4283887c2a1f5a4d7ead47c553de3e3e6c16c5a
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2024-12-11 11:18:49 +0900

    test_plotastrodata

--- test_plotastrodata.py
+++ test_plotastrodata.py
@@ -1,4 +1,10 @@
-from plotastrodata.analysis_utils import AstroData
+from plotastrodata.analysis_utils import *
+from plotastrodata.fft_utils import *
+from plotastrodata.fits_utils import *
+from plotastrodata.fitting_utils import *
+from plotastrodata.los_utils import *
+from plotastrodata.other_utils import *
+from plotastrodata.plot_utils import *
 
 
 def test_astrodata():
