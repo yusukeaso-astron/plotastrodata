c0655eca5b7182989ecb8f948462225c211a7a93
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2025-01-26 13:22:43 +0900

    update test

--- test_plotastrodata.py
+++ test_plotastrodata.py
@@ -2,7 +2,7 @@ from plotastrodata.analysis_utils import AstroData, AstroFrame
 from plotastrodata import const_utils
 from plotastrodata.fft_utils import fftcentering2, ifftcentering2
 from plotastrodata.fits_utils import FitsData, fits2data, data2fits
-from plotastrodata.fitting_utils import PTEmceeCorner
+from plotastrodata.fitting_utils import EmceeCorner
 from plotastrodata.los_utils import obs2sys, sys2obs, polarvel2losvel
 from plotastrodata.other_utils import (coord2xy, xy2coord, rel2abs, abs2rel,
                                        estimate_rms, trim, BnuT, JnuT, gaussian2d)
