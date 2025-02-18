3b3771f312b918f03fa77d6663c55c11297a9410
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2025-02-18 10:00:42 +0900

    docstrings

--- plotastrodata/plot_utils.py
+++ plotastrodata/plot_utils.py
@@ -290,24 +290,16 @@ def kwargs2PlotAxes2D(kw: dict) -> PlotAxes2D:
 class PlotAstroData(AstroFrame):
     """Make a figure from 2D/3D FITS files or 2D/3D arrays.
 
-    Basic rules --- For 3D data, a 1D velocity array or a FITS file
-    with a velocity axis must be given to set up channels in each page.
-    For 2D/3D data, the spatial center can be read from a FITS file
-    or manually given.
+    Basic rules ---
+    For 3D data, a 1D velocity array or a FITS file with a velocity axis must be given to set up channels in each page.
+    For 2D/3D data, the spatial center can be read from a FITS file or manually given.
     len(v)=1 (default) means to make a 2D figure.
     Spatial lengths are in the unit of arcsec, or au if dist (!= 1) is given.
     Angles are in the unit of degree.
-    For region, line, arrow, label, and marker,
-    a single input can be treated without a list, e.g., anglelist=60,
-    as well as anglelist=[60].
-    Each element of poslist supposes a text coordinate
-    like '01h23m45.6s 01d23m45.6s' or a list of relative x and y
-    like [0.2, 0.3] (0 is left or bottom, 1 is right or top).
-    Parameters for original methods in matplotlib.axes.Axes can be
-    used as kwargs; see the default _kw for reference.
-    Position-velocity diagrams (pv=True) does not yet suppot region, line,
-    arrow, and segment because the units of abscissa and ordinate
-    are different.
+    For region, line, arrow, label, and marker, a single input can be treated without a list, e.g., anglelist=60, as well as anglelist=[60].
+    Each element of poslist supposes a text coordinate like '01h23m45.6s 01d23m45.6s' or a list of relative x and y like [0.2, 0.3] (0 is left or bottom, 1 is right or top).
+    Parameters for original methods in matplotlib.axes.Axes can be used as kwargs; see the default _kw for reference.
+    Position-velocity diagrams (pv=True) does not yet suppot region, line, arrow, and segment because the units of abscissa and ordinate are different.
 
     kwargs is the arguments of AstroFrame to define plotting ranges.
 
