90f5a735460c8ee346169cc6df867d09897c6edf
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2025-08-04 13:04:40 +0900

    combined example with ipynb

--- example.py
+++ example.py
@@ -1,9 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+pre = '/Users/yusukeaso/C_programs/Programs/plotastrodata/testFITS/'
+
+################################################################################
 from plotastrodata.analysis_utils import AstroData, AstroFrame
 from plotastrodata.plot_utils import PlotAstroData as pad
-from plotastrodata.plot_utils import plotprofile, plotslice, plot3d
 
-
-pre = 'testFITS/'
 
 # 2D case
 d = AstroData(fitsimage=pre+'test2D.fits', Tb=True, sigma=5e-3)
@@ -78,6 +81,10 @@ p.add_region('ellipse', [0.2, 0.8], majlist=0.4, minlist=0.2, palist=45)
 p.set_axis_radec(nticksminor=5, title={'label': '2D RGB', 'loc': 'right'})
 p.savefig('test2Drgb.png', show=True)
 
+################################################################################
+from plotastrodata.plot_utils import plotprofile, plotslice, plot3d
+
+
 # Line profile
 plotprofile(fitsimage=pre+'test3D.fits', ellipse=[[0.2, 0.2, 0]] * 2, flux=True,
             coords=['04h04m43.045s 26d18m55.766s', '04h04m43.109s 26d18m56.704s'],
@@ -91,3 +98,155 @@ plotslice(length=1.6, pa=270, fitsimage=pre+'test2D.fits',
 # Rotatable 3D cube in html
 plot3d(rmax=0.8, vmin=-5, vmax=5, fitsimage=pre+'test3D.fits',
        outname='test3D', levels=[3, 6, 9], show=False)
+
+################################################################################
+import matplotlib.animation as animation
+
+
+# The following introduces a way for making an animation file of a given FITS cube.
+nchans = 31
+
+def update_plot(i):
+    print(i)
+    f = pad(rmax=0.8, fitsimage=pre+'test3D.fits', vmin=-5, vmax=5, vskip=2,
+            channelnumber=i, fig=fig)
+    f.add_color(fitsimage=pre+'test3D.fits', stretch='log')
+    f.add_scalebar(length=50 / 140, label='50 au')
+    f.set_axis_radec(grid={}, title='3D channel maps')
+    f.fig.tight_layout()
+
+fig = plt.figure()
+ani = animation.FuncAnimation(fig, update_plot, frames=nchans, interval=50)
+Writer = animation.writers['ffmpeg']  # for mp4
+# Writer = animation.writers['pillow']  # for gif
+writer = Writer(fps=10, bitrate=128)  # frame per second
+ani.save('test_animation.mp4', writer=writer)
+# ani.save('test_animation.gif', writer=writer)
+plt.close()
+
+################################################################################
+from plotastrodata.los_utils import sys2obs, polarvel2losvel
+
+
+# The following introduces a way for plotting the projected morphology and the line-of-sight velocity of a streamer.
+incl = 60
+phi0 = 0
+theta0 = 90
+
+xscale = np.sin(np.radians(theta0))**2
+vscale = 1 / np.sin(np.radians(theta0))
+
+xsys = np.linspace(0, 3 / xscale, 200)
+ysys = np.sqrt(2 * xsys + 1)
+zsys = np.zeros_like(xsys)
+
+r = np.hypot(xsys, ysys)
+theta = np.zeros_like(r) + np.pi / 2
+phi = np.arctan2(ysys, xsys)
+
+v_r = -np.sqrt(1 / r * (2 - 1 / r))
+v_theta = np.zeros_like(v_r)
+v_phi = 1 / r
+
+xsys = xsys * xscale
+ysys = ysys * xscale
+zsys = zsys * xscale
+
+v_r = v_r * vscale
+v_theta = v_theta * vscale
+v_phi = v_phi * vscale
+
+xobs, yobs, zobs = sys2obs(xsys=xsys, ysys=ysys, zsys=zsys,
+                           incl=incl, phi0=phi0, theta0=theta0)
+vlos = polarvel2losvel(v_r=v_r, v_theta=v_theta, v_phi=v_phi,
+                       theta=theta, phi=phi,
+                       incl=incl, phi0=phi0, theta0=theta0)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+m = ax.scatter(xobs, yobs, c=vlos, cmap='coolwarm', vmin=-1.5, vmax=1.5)
+fig.colorbar(m, ax=ax, label=r'$V_\mathrm{los} / \sqrt{GM_{*}/r_{c}}$')
+ax.set_xlim(3, -3)
+ax.set_ylim(-3, 3)
+ax.set_aspect(1)
+ax.set_xlabel(r'$x / r_{c}$')
+ax.set_ylabel(r'$y / r_{c}$')
+ax.grid()
+fig.savefig('streamer.png')
+plt.show()
+
+################################################################################
+from plotastrodata.fitting_utils import EmceeCorner
+from plotastrodata.plot_utils import set_rcparams
+
+
+set_rcparams(fontsize=12)
+
+def logl(p):
+    x1, x2, x3 = p
+    chi2 = (x1 / 1)**2 + (x2 / 2)**2 + (x3 / 4)**2
+    return -0.5 * chi2
+
+# initialization
+fitter = EmceeCorner(bounds=[[-5, 5], [-10, 10], [-20, 20]],
+                     logl=logl, progressbar=False, percent=[16, 84])
+
+# Using emcee, corner, and dynesty
+fitter.fit(nwalkersperdim=30, nsteps=11000, nburnin=1000,
+           savechain='chain.npy')
+print('best:', fitter.popt)
+print('lower percentile:', fitter.plow)
+print('50 percentile:', fitter.pmid)
+print('higher percentile:', fitter.phigh)
+fitter.getDNSevidence()
+print('evidence:', fitter.evidence)
+fitter.plotcorner(show=True, savefig='corner.png',
+                  labels=['par1', 'par2', 'par3'],
+                  cornerrange=[[-4, 4], [-8, 8], [-16, 16]])
+fitter.plotchain(show=True, savefig='chain.png',
+                 labels=['par1', 'par2', 'par3'],
+                 ylim=[[-2, 2], [-4, 4], [-8, 8]])
+
+# Calculating logl on a parameter grid
+fitter.posteriorongrid(ngrid=[101, 201, 401])
+print('best:', fitter.popt)
+print('lower percentile:', fitter.plow)
+print('50th percentile:', fitter.pmid)
+print('higher percentile:', fitter.phigh)
+print('evidence:', fitter.evidence)
+fitter.plotongrid(show=True, savefig='grid.png',
+                  labels=['par1', 'par2', 'par3'],
+                  cornerrange=[[-4, 4], [-8, 8], [-16, 16]])
+
+################################################################################
+from plotastrodata.fft_utils import fftcentering
+
+
+# Calculate the FFT of a boxcar function
+x = np.linspace(-99.5, 99.5, 200)
+f = np.where(np.abs(x)<10, 1, 0)
+
+u = np.fft.fftshift(np.fft.fftfreq(len(x), d=x[1] - x[0]))
+F = np.fft.fftshift(np.fft.fft(f))
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(u, np.real(F), label='real')
+ax.plot(u, np.imag(F), label='imag')
+ax.set_xlabel('u')
+ax.set_ylabel('numpy.fft')
+ax.legend()
+fig.savefig('numpyfft.png')
+plt.show()
+
+F, u = fftcentering(f=f, x=x, xcenter=0)
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(u, np.real(F), label='real')
+ax.plot(u, np.imag(F), label='imag')
+ax.set_xlabel('u')
+ax.set_ylabel('fftcentering')
+ax.legend()
+fig.savefig('fftcentering.png')
+plt.show()
