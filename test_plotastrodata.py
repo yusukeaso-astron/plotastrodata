ddaa99a2298a07f72f6854b995a650fe8b51c18d
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2026-04-17 01:36:38 +0900

    test updated

--- test_plotastrodata.py
+++ test_plotastrodata.py
@@ -14,22 +14,22 @@ is_html = False
 for i in range(len(lines)):
     s = lines[i]
     if '# Animation' in s:
-         is_animation = True
+        is_animation = True
     if '# 3D html' in s:
-         is_html = True
+        is_html = True
     if is_animation and '####' in s:
-         is_animation = False
+        is_animation = False
     if is_html and '####' in s:
-         is_html = False
+        is_html = False
     s = s.replace('show=True', 'show=False')
     s = s.replace('plt.show()', 'plt.close()')
     if is_animation or is_html:
-         s = '\n'
+        s = '\n'
     lines[i] = s
 
 with open('example_temp.py', 'w') as f:
-    for l in lines:
-        f.write(l)
+    for line in lines:
+        f.write(line)
 
 subprocess.run(shlex.split('python example_temp.py'))
 os.remove('example_temp.py')
@@ -40,7 +40,8 @@ for file in source_dir.glob('*.png'):
     file.rename(dest_dir / file.name)
 for file in glob.glob("*.mp4") + glob.glob("*.html"):
     os.remove(file)
-    
+
+
 def images_are_close(img_path1, img_path2, tolerance=0):
     img1 = Image.open(img_path1).convert("RGB")
     img2 = Image.open(img_path2).convert("RGB")
@@ -53,6 +54,7 @@ def images_are_close(img_path1, img_path2, tolerance=0):
     max_diff = np.percentile(diff, 99)
     return max_diff <= tolerance
 
+
 reslist = []
 pnglist = glob.glob("./example_data/output_expected/*.png")
 for file in pnglist:
@@ -62,6 +64,7 @@ for file in pnglist:
     reslist.append(res)
 reslist = np.array(reslist)
 pnglist = np.array(pnglist)
+
 
 def test_pngfiles():
     if np.any(reslist):
