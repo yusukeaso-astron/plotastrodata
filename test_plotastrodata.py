22d5b97558c2b7b0ff979697d63a1df6aa22092e
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2026-04-17 00:58:16 +0900

    test_plotastrodata updated

--- test_plotastrodata.py
+++ test_plotastrodata.py
@@ -1,37 +1,57 @@
-from plotastrodata import const_utils as cu
-from plotastrodata.analysis_utils import AstroData, AstroFrame
-from plotastrodata.coord_utils import (abs2rel, coord2xy,
-                                       rel2abs, xy2coord)
-from plotastrodata.ext_utils import BnuT, JnuT
-from plotastrodata.fft_utils import fftcentering2, ifftcentering2
-from plotastrodata.fits_utils import data2fits, fits2data, FitsData
-from plotastrodata.fitting_utils import (EmceeCorner,
-                                         gaussian1d, gaussian2d,
-                                         gaussfit1d, gaussfit2d)
-from plotastrodata.los_utils import obs2sys, polarvel2losvel, sys2obs
-from plotastrodata.matrix_utils import dot2d, Mfac, Mrot, Mrot3d
-from plotastrodata.noise_utils import estimate_rms
-from plotastrodata.other_utils import trim
-from plotastrodata.plot_utils import (plot3d, PlotAstroData,
-                                      plotprofile, plotslice,
-                                      set_rcparams)
+import glob
+import numpy as np
+import os
+import shlex
+import subprocess
+from pathlib import Path
+from PIL import Image
 
 
-def test_import():
-    a = [AstroData, AstroFrame,
-         abs2rel, coord2xy, rel2abs, xy2coord,
-         BnuT, JnuT,
-         fftcentering2, ifftcentering2,
-         data2fits, fits2data, FitsData,
-         EmceeCorner, gaussian1d, gaussian2d, gaussfit1d, gaussfit2d,
-         obs2sys, polarvel2losvel, sys2obs,
-         dot2d, Mfac, Mrot, Mrot3d,
-         estimate_rms,
-         trim,
-         plot3d, PlotAstroData, plotprofile, plotslice, set_rcparams
-         ]
-    d = AstroData()
-    c1 = None not in a
-    c2 = type(d.todict()) is dict
-    c3 = 299000 < cu.c_kms < 300000
-    assert c1 and c2 and c3
+with open('example.py', 'r') as f:
+    lines = f.readlines()
+for i in range(len(lines)):
+    s = lines[i]
+    s = s.replace('show=True', 'show=False')
+    s = s.replace('plt.show()', 'plt.close()')
+    lines[i] = s
+
+with open('example_temp.py', 'w') as f:
+    for l in lines:
+        f.write(l)
+
+subprocess.run(shlex.split('python example_temp.py'))
+os.remove('example_temp.py')
+
+source_dir = Path('./')
+dest_dir = Path('./example_data/output/')
+for file in source_dir.glob('*.png'):
+    file.rename(dest_dir / file.name)
+for file in glob.glob("*.mp4") + glob.glob("*.html"):
+    os.remove(file)
+    
+def images_are_close(img_path1, img_path2, tolerance=0):
+    img1 = Image.open(img_path1).convert("RGB")
+    img2 = Image.open(img_path2).convert("RGB")
+    if img1.size != img2.size:
+        return False
+
+    arr1 = np.array(img1)
+    arr2 = np.array(img2)
+    diff = np.abs(arr1.astype(int) - arr2.astype(int))
+    max_diff = np.percentile(diff, 99)
+    return max_diff <= tolerance
+
+reslist = []
+pnglist = glob.glob("./example_data/output_expected/*.png")
+for file in pnglist:
+    output = file
+    expected = file.replace('/output_expected/', '/output/')
+    res = images_are_close(output, expected, tolerance=2)
+    reslist.append(res)
+reslist = np.array(reslist)
+pnglist = np.array(pnglist)
+
+def test_pngfiles():
+    if np.any(reslist):
+        print(pnglist[reslist])
+    assert np.any(reslist)
