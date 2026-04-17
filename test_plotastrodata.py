7799dc0f7168c6bdafefd44978eafb5bf2ef6044
Yusuke Aso <98569855+yusukeaso-astron@users.noreply.github.com>
2026-04-17 10:38:22 +0900

    test updated

--- test_plotastrodata.py
+++ test_plotastrodata.py
@@ -10,20 +10,15 @@ from PIL import Image
 with open('example.py', 'r') as f:
     lines = f.readlines()
 is_animation = False
-is_html = False
 for i in range(len(lines)):
     s = lines[i]
     if '# Animation' in s:
         is_animation = True
-    if '# 3D html' in s:
-        is_html = True
     if is_animation and '####' in s:
         is_animation = False
-    if is_html and '####' in s:
-        is_html = False
     s = s.replace('show=True', 'show=False')
     s = s.replace('plt.show()', 'plt.close()')
-    if is_animation or is_html:
+    if is_animation:
         s = '\n'
     lines[i] = s
 
@@ -36,9 +31,10 @@ os.remove('example_temp.py')
 
 source_dir = Path('./')
 dest_dir = Path('./example_data/output/')
-for file in source_dir.glob('*.png'):
-    file.rename(dest_dir / file.name)
-for file in glob.glob("*.mp4") + glob.glob("*.html"):
+for pattern in ('*.png', '*.html'):
+    for file in source_dir.glob(pattern):
+        file.rename(dest_dir / file.name)
+for file in glob.glob("*.mp4"):
     os.remove(file)
 
 
@@ -62,11 +58,19 @@ for file in pnglist:
     expected = file.replace('/output_expected/', '/output/')
     res = images_are_close(output, expected, tolerance=2)
     reslist.append(res)
-reslist = np.array(reslist)
-pnglist = np.array(pnglist)
 
+htmllist = glob.glob("./example_data/output_expected/*.html")
+for file in htmllist:
+    output = file
+    expected = file.replace('/output_expected/', '/output/')
+    with open(output) as f1, open(expected) as f2:
+        res = (f1.read() == f2.read())
+    reslist.append(res)
+filelist = np.concatenate((pnglist, htmllist))
+reslist = np.array(reslist)
 
 def test_pngfiles():
     if np.any(reslist):
-        print(pnglist[reslist], 'are different from the expected files.')
+        print('Mismatched files:')
+        print(filelist[reslist])
     assert np.any(reslist)
